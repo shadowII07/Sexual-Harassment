91d2d73c49e3e83a1a7f0a4aa3487d944130c163
shadowII07 <israelmarcos073002@gmail.com>
2025-04-01 12:17:26 -0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Function to show an alert when a resource link is clicked
-function showAlert() {
-    alert("If you or someone you know is in need of help, please reach out to a trusted adult or contact the appropriate hotline.");
-}
